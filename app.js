13db68c5e767f94a2d3066dee0faa333fbbc641e
chathulks <sadeeshachathumal5@gmail.com>
2026-08-15 10:25:39 +0530

    Fix input IDs in index.html for email and full name fields to match JavaScript references

---
+++ app.js
@@ -0,0 +1,14 @@
+function OnclickNewUser(){
+
+    let u_id = document.getElementById("u_id").value;
+    let u_id = document.getElementById("u_id").value;
+    let u_id = document.getElementById("u_id").value;
+    let u_id = document.getElementById("u_id").value;
+
+    let user = {
+        "userId": u_id,
+        "emailId": u_email,
+        "fullName": u_fulln,
+        "password": u_pw
+    }
+}
