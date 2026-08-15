3c9f148624a6ca39c5dff9b824453de5396bcdfe
chathulks <sadeeshachathumal5@gmail.com>
2026-08-15 10:29:01 +0530

    Refactor OnclickNewUser function to use const for user object and improve formatting

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-function OnclickNewUser(){
+function OnclickNewUser() {
 
     let u_id = document.getElementById("u_id").value;
     let u_email = document.getElementById("u_email").value;
     let u_fulln = document.getElementById("u_fulln").value;
     let u_pw = document.getElementById("u_pw").value;
 
-    let user = {
+    const user = {
         "userId": u_id,
         "emailId": u_email,
         "fullName": u_fulln,
         "password": u_pw
     }
 
-    
+    fetch('https://api.freeprojectapi.com/api/UserApp/CreateNewUser', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    })
+        .then(response => response.json())
+        .then(data => console.log(data));
+
 }
