98acb468c4efcbcdedd7e66988e59dedc5ccedf7
chathulks <sadeeshachathumal5@gmail.com>
2026-08-15 10:26:56 +0530

    Fix variable assignments in OnclickNewUser function to correctly retrieve user input values

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 function OnclickNewUser(){
 
     let u_id = document.getElementById("u_id").value;
-    let u_id = document.getElementById("u_id").value;
-    let u_id = document.getElementById("u_id").value;
-    let u_id = document.getElementById("u_id").value;
+    let u_email = document.getElementById("u_email").value;
+    let u_fulln = document.getElementById("u_fulln").value;
+    let u_pw = document.getElementById("u_pw").value;
 
     let user = {
         "userId": u_id,
@@ -11,4 +11,6 @@ function OnclickNewUser(){
         "fullName": u_fulln,
         "password": u_pw
     }
+
+    
 }
