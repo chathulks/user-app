c6f7cadf6d638dbb0b4c658aef371d2dcd84e32e
chathulks <sadeeshachathumal5@gmail.com>
2026-08-22 20:07:15 +0530

    Remove logging of email, token, and refresh token on successful sign-in

--- app.js
+++ app.js
@@ -106,9 +106,6 @@ function onClickSignin() {
         .then(response => response.json())
         .then(data => {
             if (data.result == true) {
-                console.log(data.data.emailId);
-                console.log(data.data.token);
-                console.log(data.data.refreshToken);
                 window.location.href = "/usermange.html";
             } else {
                 console.log("Invalid Details");
